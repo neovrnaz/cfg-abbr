d36ad19e8daa047129c67bd77db413a2bc1576b7
neovrnaz <60867530+neovrnaz@users.noreply.github.com>
2022-05-10 15:27:42 -0600

    Update cfg-abbr.fish

--- cfg-abbr.fish
+++ cfg-abbr.fish
@@ -1,94 +1,94 @@
 set -g __neovrnaz_cfg_abbr_version 0.0.1
 
-abbr config '/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
+abbr -a -U -- config '/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
 
-abbr c 'config'
+abbr -a -U -- c 'config'
   
-abbr ca 'config add'
-abbr caa 'config add --all'
+abbr -a -U -- ca 'config add'
+abbr -a -U -- caa 'config add --all'
   
-abbr cb 'config branch'
-abbr cbd 'config branch -D'
+abbr -a -U -- cb 'config branch'
+abbr -a -U -- cbd 'config branch -D'
   
-abbr cbl 'config blame'
+abbr -a -U -- cbl 'config blame'
   
-abbr cc 'config commit -v'
-abbr cc! 'config commit -v --amend'
-abbr ccn! 'config commit -v --amend --no-edit'
-abbr cca 'config commit -a -v'
-abbr cca! 'config commit -a -v --amend'
-abbr ccan! 'config commit -a -v --no-edit --amend'
-abbr ccans! 'config commit -a -v -s --no-edit --amend'
+abbr -a -U -- cc 'config commit -v'
+abbr -a -U -- cc! 'config commit -v --amend'
+abbr -a -U -- ccn! 'config commit -v --amend --no-edit'
+abbr -a -U -- cca 'config commit -a -v'
+abbr -a -U -- cca! 'config commit -a -v --amend'
+abbr -a -U -- ccan! 'config commit -a -v --no-edit --amend'
+abbr -a -U -- ccans! 'config commit -a -v -s --no-edit --amend'
 
-abbr ccl 'config clone --recursive'
+abbr -a -U -- ccl 'config clone --recursive'
 
-abbr ccf 'config config --list'
+abbr -a -U -- ccf 'config config --list'
 
-abbr cclean 'config clean -fd'
+abbr -a -U -- cclean 'config clean -fd'
 
-abbr cco 'config checkout'
-abbr ccob 'config checkout -b'
-abbr ccom 'config checkout master'
-abbr ccod 'config checkout develop'
-abbr ccof 'config checkout feat/'
+abbr -a -U -- cco 'config checkout'
+abbr -a -U -- ccob 'config checkout -b'
+abbr -a -U -- ccom 'config checkout master'
+abbr -a -U -- ccod 'config checkout develop'
+abbr -a -U -- ccof 'config checkout feat/'
 
-abbr ccp 'config cherry-pick'
-abbr ccpa 'config cherry-pick --abort'
-abbr ccpc 'config cherry-pick --continue'
+abbr -a -U -- ccp 'config cherry-pick'
+abbr -a -U -- ccpa 'config cherry-pick --abort'
+abbr -a -U -- ccpc 'config cherry-pick --continue'
 
-abbr cd 'config diff'
+abbr -a -U -- cd 'config diff'
 
-abbr cf 'config fetch'
-abbr cfa 'config fetch --all --prune'
-abbr cfo 'config fetch origin'
+abbr -a -U -- cf 'config fetch'
+abbr -a -U -- cfa 'config fetch --all --prune'
+abbr -a -U -- cfo 'config fetch origin'
 
 
-abbr cl 'config log'
-abbr clg 'config log --graph'
+abbr -a -U -- cl 'config log'
+abbr -a -U -- clg 'config log --graph'
 
-abbr cm 'config merge'
+abbr -a -U -- cm 'config merge'
   
-abbr cp 'config push'
-abbr cpf 'config push --force'
-abbr cpt 'config push --tags'
-abbr cptf 'config push --tags --force'
-abbr cpoat 'config push origin --all && git push origin --tags'
-abbr cpoatf 'config push origin --all -f && git push origin --tags -f'
+abbr -a -U -- cp 'config push'
+abbr -a -U -- cpf 'config push --force'
+abbr -a -U -- cpt 'config push --tags'
+abbr -a -U -- cptf 'config push --tags --force'
+abbr -a -U -- cpoat 'config push origin --all && git push origin --tags'
+abbr -a -U -- cpoatf 'config push origin --all -f && git push origin --tags -f'
 
-abbr cpristine 'config reset --hard && git clean -dfx'
+abbr -a -U -- cpristine 'config reset --hard && git clean -dfx'
 
-abbr cpl 'config pull'
-abbr cpo 'config pull origin'
-abbr cpom 'config pull origin master'
-abbr cpu 'config pull upstream'
-abbr cpum 'config pull upstream master'
+abbr -a -U -- cpl 'config pull'
+abbr -a -U -- cpo 'config pull origin'
+abbr -a -U -- cpom 'config pull origin master'
+abbr -a -U -- cpu 'config pull upstream'
+abbr -a -U -- cpum 'config pull upstream master'
 
-abbr cr 'config remote -v'
-abbr cra 'config remote add'
-abbr crau 'config remote add upstream'
-abbr crrm 'config remote remove'
-abbr crmv 'config remote rename'
-abbr crset 'config remote set-url'
+abbr -a -U -- cr 'config remote -v'
+abbr -a -U -- cra 'config remote add'
+abbr -a -U -- crau 'config remote add upstream'
+abbr -a -U -- crrm 'config remote remove'
+abbr -a -U -- crmv 'config remote rename'
+abbr -a -U -- crset 'config remote set-url'
 
-abbr crb 'config rebase'
-abbr crba 'config rebase --abort'
-abbr crbc 'config rebase --continue'
+abbr -a -U -- crb 'config rebase'
+abbr -a -U -- crba 'config rebase --abort'
+abbr -a -U -- crbc 'config rebase --continue'
 
-abbr crt 'config reset HEAD'
-abbr crhh 'config reset HEAD --hard'
-abbr crth 'config reset --hard'
+abbr -a -U -- crt 'config reset HEAD'
+abbr -a -U -- crhh 'config reset HEAD --hard'
+abbr -a -U -- crth 'config reset --hard'
 
-abbr cst 'config status'
-abbr css 'config status -s'
+abbr -a -U -- cst 'config status'
+abbr -a -U -- css 'config status -s'
 
-abbr css 'config stash save'
-abbr csa 'config stash apply'
-abbr csd 'config stash drop'
-abbr csp 'config stash pop'
+abbr -a -U -- css 'config stash save'
+abbr -a -U -- csa 'config stash apply'
+abbr -a -U -- csd 'config stash drop'
+abbr -a -U -- csp 'config stash pop'
 
-abbr csu 'config submodule update'
+abbr -a -U -- csu 'config submodule update'
 
-abbr cts 'config tag -s'
+abbr -a -U -- cts 'config tag -s'
 
 
 set -l name (basename (status -f) .fish){_uninstall}
